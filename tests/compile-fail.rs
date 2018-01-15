4cf38a758e6f268264c9aa1f34ee32268d32afc2
Jonas Schievink <jonasschievink@gmail.com>
2018-01-15 01:47:49 +0100

    Add note to wrapper test

--- tests/compile-fail.rs
+++ tests/compile-fail.rs
@@ -1,3 +1,7 @@
 #[macro_use] extern crate compile_fail;
 
 run_tests!();
+
+// These 2 lines are already enough for the common configuration.
+// It will generate a `compile_fail` test function that will run all compile-fail tests from the
+// `tests/compile-fail` directory.
